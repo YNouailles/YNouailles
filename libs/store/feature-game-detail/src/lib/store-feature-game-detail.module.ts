9171e779dfed94d1d087612047b7a8942d502b8d
YNouailles <ynouailles@slb.com>
2022-09-26 15:21:55 +0200

    lab 9

--- libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { GameDetailComponent } from './game-detail/game-detail.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
@@ -12,6 +13,7 @@ import { GameDetailComponent } from './game-detail/game-detail.component';
       { path: '', pathMatch: 'full', component: GameDetailComponent },
     ]),
     MatCardModule,
+    MatButtonModule,
   ],
   declarations: [GameDetailComponent],
 })
